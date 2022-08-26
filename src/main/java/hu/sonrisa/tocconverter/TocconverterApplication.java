5ca7893f9703e17c4e29942744e6395aa22857ff
Zoltan Felso <felso.zoltan@sonrisa.hu>
2022-08-26 13:24:08 +0200

    Project initialization (by start.spring.io)

---
+++ src/main/java/hu/sonrisa/tocconverter/TocconverterApplication.java
@@ -0,0 +1,13 @@
+package hu.sonrisa.tocconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TocconverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TocconverterApplication.class, args);
+	}
+
+}
