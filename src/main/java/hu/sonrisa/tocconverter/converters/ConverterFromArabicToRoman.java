7bf8e97634f88d0ebd73ed35097ed5ffe42c1f2a
Zoltan Felso <felso.zoltan@sonrisa.hu>
2022-08-29 08:13:22 +0200

    I tried to improve the performance by accelerator board, but in vain. I wil remove it

--- src/main/java/hu/sonrisa/tocconverter/converters/ConverterFromArabicToRoman.java
+++ src/main/java/hu/sonrisa/tocconverter/converters/ConverterFromArabicToRoman.java
@@ -2,13 +2,31 @@ package hu.sonrisa.tocconverter.converters;
 
 import hu.sonrisa.tocconverter.BusinesException;
 
+import java.util.Map;
+
+import static java.util.Map.entry;
+
 public class ConverterFromArabicToRoman {
 
     private final CharSequence[] ones = {"III", "XXX", "CCC", "MMM"};
     private final char[] fives = {'V', 'L', 'D'};
     private final char[] tens = {'X', 'C', 'M'};
+    private final Map<String, String> table = Map.ofEntries(
+        entry("1", "I"),
+        entry("2", "II"),
+        entry("3", "III"),
+        entry("4", "IV"),
+        entry("5", "V"),
+        entry("6", "VI"),
+        entry("7", "VII"),
+        entry("8", "VIII"),
+        entry("9", "IX"));
 
     public String convert(String arabicNumber) {
+        String r = table.get(arabicNumber);
+        if (r != null) {
+            return r;
+        }
         InputInverseSplitter splitter = new InputInverseSplitter(arabicNumber.trim());
         char actChar = splitter.getNextChar();
         int position = 0;
